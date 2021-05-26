fca615062a5abdeac60c9913551bda1d784ff1dc
rahmadani325 <rahmadaniburhan328@gmail.com>
2021-05-26 13:03:03 +0800

    Set UI awal untuk main activity

--- app/src/main/java/com/example/tugasroom/ui/main/MainActivity.kt
+++ app/src/main/java/com/example/tugasroom/ui/main/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.tugasroom
+package com.example.tugasroom.ui.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.tugasroom.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
