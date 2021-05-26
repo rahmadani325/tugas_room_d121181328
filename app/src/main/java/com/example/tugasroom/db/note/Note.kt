e5cc64de571db3b40a04217ac0adc0e7717686d9
rahmadani325 <rahmadaniburhan328@gmail.com>
2021-05-26 13:16:09 +0800

    Step 2 => Membuat struktur table note di Note.kt

---
+++ app/src/main/java/com/example/tugasroom/db/note/Note.kt
@@ -0,0 +1,17 @@
+package com.example.tugasroom.db.note
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+
+@Entity(tableName = "note")
+data class Note(
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "id")
+    val id: Int? = null,
+
+    @ColumnInfo(name = "note")
+
+    var note: String
+)
