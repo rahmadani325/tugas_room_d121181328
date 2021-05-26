c9e4902a399219574bf5a9c8e28fb519fee03073
rahmadani325 <rahmadaniburhan328@gmail.com>
2021-05-26 13:33:31 +0800

    Step 4 => Membuat ViewModel

---
+++ app/src/main/java/com/example/tugasroom/ui/main/NoteViewModel.kt
@@ -0,0 +1,31 @@
+package com.example.tugasroom.ui.main
+
+import NoteRepository
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import com.example.tugasroom.db.note.Note
+
+
+class NoteViewModel(application: Application) : AndroidViewModel(application) {
+
+    private var noteRepository = NoteRepository(application)
+    private var notes: LiveData<List<Note>>? = noteRepository.getNotes()
+
+    fun insertNote(note: Note) {
+        noteRepository.insert(note)
+    }
+
+    fun getNotes(): LiveData<List<Note>>? {
+        return notes
+    }
+
+    fun deleteNote(note: Note) {
+        noteRepository.delete(note)
+    }
+
+    fun updateNote(note: Note) {
+        noteRepository.update(note)
+    }
+
+}
